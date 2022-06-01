9f44840313b8c870c8d0e4c8802efcdae7bdd5d0
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-06-01 00:11:03 -0600

    tests

--- cypress/integration/webTests.js
+++ cypress/integration/webTests.js
@@ -1,28 +1,27 @@
-describe("Pizza Form", ()=> {
-    beforeEach(() => {
-        cy.visit("http://localhost:3000/pizza");
-    })
+describe("Pizza Form", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/pizza");
+  });
 
-    const nameInput = () => cy.get("input[name=name]");
-    const toppingsInput = () => cy.get(".topping");
-    const submitButton = () => cy.get(`button[id="order-button"]`);
-    const submittedPizza = () => cy.get("#your-pizza");
+  const nameInput = () => cy.get("input[name=name]");
+  const toppingsInput = () => cy.get(".topping");
+  const submitButton = () => cy.get(`button[id="order-button"]`);
+  const submittedPizza = () => cy.get("#your-pizza");
 
-    it("Can type in the name input", () =>{
-        nameInput()
-            .should('have.value', '')
-            .type('Esmodea')
-            .should('have.value', 'Esmodea')
-    })
+  it("Can type in the name input", () => {
+    nameInput()
+      .should("have.value", "")
+      .type("Anne")
+      .should("have.value", "Anne");
+  });
 
-    it("Has multiple selectable toppings", () =>{
-        toppingsInput()
-            .should('have.length', 4)
-    })
-    
-    it("Can be submitted", () =>{
-        nameInput().type("Darth Vader");
-        submitButton().click();
-        submittedPizza().contains('Darth Vader');
-    })
-})
+  it("Has multiple selectable toppings", () => {
+    toppingsInput().should("have.length", 4);
+  });
+
+  it("Can be submitted", () => {
+    nameInput().type("Darth Vader");
+    submitButton().click();
+    submittedPizza().contains("Darth Vader");
+  });
+});
