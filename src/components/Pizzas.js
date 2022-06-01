6bd86d3906e43133622b222e811bc0484f0fc699
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-05-31 23:53:22 -0600

    added css

--- src/components/Pizzas.js
+++ src/components/Pizzas.js
@@ -1,20 +1,27 @@
 import React from "react";
 
-const Pizza = props =>{
-    const {values} = props;
+const Pizza = (props) => {
+  const { values } = props;
 
-    if(values.name){ return (
-        <div id="your-pizza">
-            <p>{`${values.name}'s Pizza`}</p>
-            <p>Size: {values.dropdown}</p>
-            <p>Toppings: {`${values.pepperoni ? 'Pepperoni ' : ''}${values.mush ? 'Mushrooms ' : ''}${values.fetta ? 'Fetta Cheese ' : ''}${values.italian ? 'Italian Sausage ' : ''}`}</p>
-            <p>Special Instructions: {`${values.special}`}</p>
-        </div>
-    )}else {
-        return (
-            <p>Order Your Pizza Now!</p>
-        )
-    }
-}
+  if (values.name) {
+    return (
+      <div id="your-pizza">
+        <p>{`${values.name}'s Pizza`}</p>
+        <p>Size: {values.dropdown}</p>
+        <p>
+          Toppings:{" "}
+          {`${values.pepperoni ? "Pepperoni " : ""}${
+            values.mush ? "Mushrooms " : ""
+          }${values.fetta ? "Fetta Cheese " : ""}${
+            values.italian ? "Italian Sausage " : ""
+          }`}
+        </p>
+        <p>Special Instructions: {`${values.special}`}</p>
+      </div>
+    );
+  } else {
+    return <p>Order Your Pizza!</p>;
+  }
+};
 
 export default Pizza;
